67ed8b9c4494aee5b94a0a6aba088aceecba2f50
max7290599 <atroshschanka.maksim@gmail.com>
2021-03-14 14:56:43 +0300

    refactor: add flat

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ module.exports = function towelSort(matrix) {
   for (let i = 0; i < matrix.length; i++) {
     if (i % 2) matrix[i].reverse();
   }
-  return (array.concat(...matrix));
+  return  matrix.flat();
 }
